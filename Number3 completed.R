dcbfd6d02158534ae8fb020aecbc58ec56f12b3e
erinkenwilliams <31249509+erinkenwilliams@users.noreply.github.com>
2017-10-31 09:27:45 -0500

    Number 4 Questions 1,2,3

---
+++ Number3 completed.R
@@ -0,0 +1,39 @@
+#load library
+library(tidyverse)
+
+#load data
+dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistories_v2.txt"), 
+                sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
+
+#rest of your code goes here
+
+
+Number 3 Part 1
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  
+  geom_point()
+
+Part 2
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  
+  scale_x_log10() +
+  
+  scale_y_log10() +
+  
+  geom_point()
+
+Part 3
+
+ggplot(data = dat, aes(x = mass.g., y = newborn.g., color = order)) +
+  
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  
+  scale_x_log10() +
+  
+  scale_y_log10() +
+  
+  geom_point()
